782ce8cb6bab49ba1ba8f04c3a34dcd35bf9ddb5
akumoth <rdppetrizzo@gmail.com>
2023-02-09 21:25:36 -0400

    EDA de world population prospects

--- flujos_migratorios/dataset_normalization.py
+++ flujos_migratorios/dataset_normalization.py
@@ -2,8 +2,12 @@ import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 
+# Importación de datos
+
 wpp_df = pd.read_excel("../datasets/raw/world_population_prospects/WPP2022_GEN_F01_DEMOGRAPHIC_INDICATORS_COMPACT_REV1.xlsx", index_col=0)
 wpp_df.drop(['Variant','Notes','Location code','ISO3 Alpha-code','ISO2 Alpha-code','SDMX code**','Parent code'],axis=1,inplace=True)
+
+# Normalización de columnas
 
 wpp_columns = wpp_df.columns.to_series()
 wpp_columns[0] = 'Region'
@@ -15,6 +19,8 @@ wpp_columns = wpp_columns.str.replace(')','_',regex=False)
 wpp_df.columns = wpp_columns
 wpp_df.region = wpp_df.region.astype(str)
 wpp_df.type = wpp_df.type.astype(str)
+
+# División en dos DF (uno de regiones, y otro según grupos de desarrollo) y conversión de tipos de datos
 
 reg_wpp_df = wpp_df[(wpp_df.type != 'World') & (wpp_df.type != 'Label/Separator') & (wpp_df.type != 'Development Group') & (wpp_df.type != 'Income Group') & (wpp_df.type != 'Special other')]
 dev_wpp_df = wpp_df[(wpp_df.type == 'Development Group') | (wpp_df.type == 'Income Group') | (wpp_df.type == 'Special other')]
