3586f64518023b7ad12325888dfb2436d1190210
akumoth <rdppetrizzo@gmail.com>
2023-02-13 21:43:25 -0400

    Revisión dataset WPP

---
+++ flujos_migratorios/normalization.py
@@ -0,0 +1,35 @@
+import pycountry
+def normalize_country(name):
+    custom_mapping = {
+        "Iran (Islamic Republic of)": "iran",
+        "côte d'ivoire": "cote d'ivoire",
+        "China, Hong Kong SAR*": "hong kong",
+        "hong kong sar, china": "hong kong",
+        "iran, islamic rep.": "iran"
+    }
+    
+    if name in custom_mapping:
+        return custom_mapping[name]
+    try:
+        country = pycountry.countries.lookup(name)
+        return country.name
+    except LookupError:
+        if name == "United States of America*":
+            return "United States"
+        elif name == "Iran (Islamic Republic of)":
+            return "iran"
+        elif name == "Russian Federation":
+            return "Russia"
+        elif name == "France*":
+            return "France"
+        elif name == "Australia*":
+            return "Australia"
+        elif name == "United Kingdom*":
+            return "United Kingdom"
+        elif name == "China, Hong Kong SAR*":
+            return "hong kong"                 
+        elif name == "Ukraine*":
+            return "Ukraine"
+        # agrega más verificaciones aquí para otros casos especiales
+        else:
+          return name
