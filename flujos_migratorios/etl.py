ac05996c1b75e1248588d0073641ff0135406218
akumoth <rdppetrizzo@gmail.com>
2023-02-21 22:31:33 -0400

    Limpieza de los csv de pp, carga adicional SQL

--- flujos_migratorios/etl.py
+++ flujos_migratorios/etl.py
@@ -6,9 +6,9 @@ import re
 from geopy.geocoders import Nominatim
 import geonamescache
 
-country_code_df = pd.read_csv('../datasets/processed/codigo_pais.csv')
+country_code_df = pd.read_csv('../datasets/processed/codigo_pais.csv').drop('Unnamed: 0',axis=1)
 country_code_df['cod'] = country_code_df['cod'].astype(int)
-country_code_df.columns = ['name','lang','cod']
+country_code_df.columns = ['name','lang','cod','lat','long']
 country_code_df = country_code_df.sort_values('cod').reset_index(drop=True)
 country_code_df.index = np.arange(1, len(country_code_df) + 1)
 country_code_df = country_code_df.drop('cod',axis=1).reset_index(names='cod')
@@ -101,8 +101,8 @@ def insert_lat_long(df):
             longitude = direction.longitude
         lat.append(latitude)
         log.append(longitude)
-    df['latitude'] = pd.Series(lat)
-    df['longitude'] = pd.Series(log)
+    df.insert(df.shape[1],'latitude',pd.Series(lat))
+    df.insert(df.shape[1],'longitude',pd.Series(log))
 
 def obtener_idioma_principal(pais):
     gc = geonamescache.GeonamesCache()
@@ -116,39 +116,28 @@ def obtener_idioma_principal(pais):
 
 economia = [
     "adjusted net national income (current us$)",
-    "adjusted net national income per capita (annual % growth)",
-    "adjusted savings: gross savings (% of gni)",
-    "cost of business start-up procedures (% of gni per capita)",
     "cost of business start-up procedures, female (% of gni per capita)",
     "cost of business start-up procedures, male (% of gni per capita)",
-    "domestic credit to private sector by banks (% of gdp)",
     "domestic credit to private sector (% of gdp)",
     "ease of doing business score (0 = lowest performance to 100 = best performance)",
     "foreign direct investment, net (bop, current us$)",
     "gdp (current us$)",
-    "gdp growth (annual %)",
     "gini index",
     "gni (current us$)",
-    "gni growth (annual %)",
     "gross domestic savings (current us$)",
-    "gross domestic savings (% of gdp)",
     "gross savings (% of gdp)",
     "gross savings (% of gni)",
     "industry (including construction), value added (% of gdp)",
-    "industry (including construction), value added (annual % growth)",
     "inflation, consumer prices (annual %)",
     "investment in ict with private participation (current us$)",
     "labor force, total",
     "labor tax and contributions (% of commercial profits)",
     "listed domestic companies, total",
     "market capitalization of listed domestic companies (current us$)",
-    "market capitalization of listed domestic companies (% of gdp)",
     "multidimensional poverty headcount ratio (% of total population)",
     "net trade in goods and services (bop, current us$)",
-    "new business density (new registrations per 1,000 people ages 15-64)",
     "new businesses registered (number)",
     "personal remittances, received (current us$)",
-    "personal remittances, received (% of gdp)",
     "personal remittances, paid (current us$)",
     "profit tax (% of commercial profits)",
     "research and development expenditure (% of gdp)",
@@ -157,8 +146,6 @@ economia = [
     "tax revenue (% of gdp)",
     "taxes on income, profits and capital gains (% of total taxes)",
     "time spent dealing with the requirements of government regulations (% of senior management time)",
-    "unemployment, total (% of total labor force) (national estimate)",
-    "unemployment, total (% of total labor force) (modeled ilo estimate)",
 ]
 
 salud = [
@@ -177,7 +164,6 @@ salud = [
     'female mortality between age 15 and 50 (deaths under age 50 per 1,000 females alive at age 15)',
     'infant deaths, under age 1 (thousands)',
     'under-five deaths, under age 5 (thousands)',
-    'infant mortality rate (infant deaths per 1,000 live births)',
     'mean age childbearing (years)',
     "domestic private health expenditure per capita (current us$)",
     "domestic general government health expenditure per capita (current us$)",
@@ -202,15 +188,12 @@ empleos = [
     "employment in agriculture (% of total employment) (modeled ilo estimate)",
     "employment in industry (% of total employment) (modeled ilo estimate)",
     "employment in services (% of total employment) (modeled ilo estimate)",
-    "firms offering formal training (% of firms)",
     "self-employed, total (% of total employment) (modeled ilo estimate)",
-    "time required to start a business (days)",
     "time required to register property (days)",
     "time required to start a business, female (days)",
     "time required to start a business, male (days)",
     "labor force, total",
     "unemployment, total (% of total labor force) (national estimate)",
-    "unemployment, total (% of total labor force) (modeled ilo estimate)",
 ]
 
 poblacion = [
@@ -219,20 +202,16 @@ poblacion = [
     'median age, as of 1 july (years)',
     "individuals using the internet (% of population)",
     "international migrant stock, total",
-    "international migrant stock (% of population)",
     'net number of migrants (thousands)',
     'net migration rate (per 1,000 population)',
     "population ages 0-14 (% of total population)",
     "population density (people per sq. km of land area)",
     "population growth (annual %)",
     "population in largest city",
-    "population in the largest city (% of urban population)",
     "refugee population by country or territory of asylum",
     "refugee population by country or territory of origin",
     "rural population (% of total population)",
-    "rural population growth (annual %)",
     "urban population (% of total population)",
-    "urban population growth (annual %)",
     "multidimensional poverty headcount ratio (% of total population)",
 ]
 
